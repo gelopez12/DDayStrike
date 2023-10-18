144a7a80ce2c2ed7eec870a202f87095baf75b98
Jaxdeyoung <148388678+Jaxdeyoung@users.noreply.github.com>
2023-10-18 16:52:16 -0500

    Player turning

--- DDayStrike/Assets/Scripts/PlayerController.cs
+++ DDayStrike/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     float speed = 10.0f;
+    float turnSpeed = 100.0f;
     public float horizontalInput;
     public float verticalInput;
 
@@ -21,6 +22,6 @@ public class PlayerController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
-
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
