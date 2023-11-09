623f46e39205b379f3bc2648901c016d1e81276d
Jaxdeyoung <148388678+Jaxdeyoung@users.noreply.github.com>
2023-11-08 19:26:01 -0600

    rigid body, colliders, and mouse

--- DDayStrike/Assets/Scripts/PlayerController.cs
+++ DDayStrike/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float speed = 10.0f;
-    //float turnSpeed = 100.0f;
+    public float speed = 10.0f;
+    public float turnSpeed = 500.0f;
+    public float angle;
     public float horizontalInput;
     public float verticalInput;
 
@@ -22,6 +23,13 @@ public class PlayerController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
-        //transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y));
+
+        Vector3 direction = mousePosition - transform.position;
+        direction.y = 0;
+        if (direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        }
     }
 }
