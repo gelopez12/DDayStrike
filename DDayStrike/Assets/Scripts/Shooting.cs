417a8e3c65de9a632a8df9f0632ac3f4b78cb44e
gelopez12 <148307276+gelopez12@users.noreply.github.com>
2023-11-25 17:00:35 -0600

    was trying to add muzzle flash

--- DDayStrike/Assets/Scripts/Shooting.cs
+++ DDayStrike/Assets/Scripts/Shooting.cs
@@ -13,21 +13,29 @@ public class Shooting : MonoBehaviour
     public AudioSource reloadPing;
     public AudioSource loadingClip;
     private float Mag = 10.0f;
+    public ParticleSystem flashMu;
+
+    private Renderer muzzleRenderer;
+    private bool isFlickering = false;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        flashMu.Stop();
+        muzzleRenderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        flashMu.Stop();
         if (Input.GetKeyDown(KeyCode.Mouse0) && Mag > 0)
         {
             Shoot();
             shoot.Play();
             Mag--;
+
 
             if (Mag == 0)
             {
@@ -39,10 +47,35 @@ public class Shooting : MonoBehaviour
         {
             StartCoroutine(ReloadCoroutine());
         }
+        
+    }
+    IEnumerator Flicker(float duration)
+    {
+        float flickerStartTime = Time.time;
+
+        while (Time.time - flickerStartTime < duration)
+        {
+            isFlickering = !isFlickering;
+
+            // Toggle the visibility of the Renderer component
+            if (muzzleRenderer != null)
+            {
+                muzzleRenderer.enabled = isFlickering;
+            }
+
+            yield return null;
+        }
+
+        // Ensure the Renderer is turned off after the flickering duration
+        if (muzzleRenderer != null)
+        {
+            muzzleRenderer.enabled = false;
+        }
     }
 
     IEnumerator ReloadCoroutine()
-    {       
+    {   
+      
         loadingClip.Play();
         
         yield return new WaitForSeconds(2.5f);
@@ -52,6 +85,8 @@ public class Shooting : MonoBehaviour
 
     void Shoot()
     {
+        flashMu.Play();
+        StartCoroutine(Flicker(0.1f));
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
         rb.AddForce(firePoint.forward * bulletForce, ForceMode.Impulse);
