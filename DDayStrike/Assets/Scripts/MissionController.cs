f08f9e0290c0a0a05e53540c43838c82fb113dff
Jaxdeyoung <148388678+Jaxdeyoung@users.noreply.github.com>
2023-12-04 19:53:46 -0600

    Spawn Ring

--- DDayStrike/Assets/Scripts/MissionController.cs
+++ DDayStrike/Assets/Scripts/MissionController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MissionController : MonoBehaviour
 {
+    public GameObject Ring;
+    public Vector3 spawnPos = new Vector3(13f, 3.38f, 145f);
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(Ring, spawnPos, Quaternion.Euler(0,0,90));
     }
 
     // Update is called once per frame
