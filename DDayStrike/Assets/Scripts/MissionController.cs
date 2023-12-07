22013b1a384bf90e345aa07d67f327cf638e7073
Jaxdeyoung <148388678+Jaxdeyoung@users.noreply.github.com>
2023-12-06 18:31:12 -0600

    Random ring spawn

--- DDayStrike/Assets/Scripts/MissionController.cs
+++ DDayStrike/Assets/Scripts/MissionController.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class MissionController : MonoBehaviour
 {
     public GameObject Ring;
-    public Vector3 spawnPos = new Vector3(13f, 3.38f, 145f);
+    public float xPos; //12f to 50f
+    public float yPos = 3.38f;
+    public float zPos; //35f to 165f
+    public Vector3 spawnPos;
     
     // Start is called before the first frame update
     void Start()
     {
+        spawnPos = new Vector3(Random.Range(12.0f, 50.0f), yPos, Random.Range(35.0f, 165.0f));
         Instantiate(Ring, spawnPos, Quaternion.Euler(0,0,90));
     }
 
