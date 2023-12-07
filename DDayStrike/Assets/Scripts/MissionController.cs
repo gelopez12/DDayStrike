2603d22d3dc3cc4cb72df579fffb1a9bdef3feac
Jaxdeyoung <148388678+Jaxdeyoung@users.noreply.github.com>
2023-12-06 19:49:25 -0600

    A lot of ring stuff

--- DDayStrike/Assets/Scripts/MissionController.cs
+++ DDayStrike/Assets/Scripts/MissionController.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class MissionController : MonoBehaviour
 {
+    public GameObject DownPlayer;
     public GameObject Ring;
-    public float xPos; //12f to 50f
-    public float yPos = 3.38f;
-    public float zPos; //35f to 165f
-    public Vector3 spawnPos;
-    
+    public GameObject ReviveRing;
+    public float ringXPos; //12f to 50f
+    public float ringYPos = 3.38f;
+    public float ringZPos; //35f to 165f
+    public Vector3 ringSpawnPos;
+    public float downXPos;
+    public float downYPos;
+    public float downZPos;
+    public Vector3 downPos;
+    public bool inReviveRing;
+
     // Start is called before the first frame update
     void Start()
     {
-        spawnPos = new Vector3(Random.Range(12.0f, 50.0f), yPos, Random.Range(35.0f, 165.0f));
-        Instantiate(Ring, spawnPos, Quaternion.Euler(0,0,90));
+        //Testing Ring Spawn
+        ringSpawnPos = new Vector3(Random.Range(12.0f, 50.0f), ringYPos, Random.Range(35.0f, 165.0f));
+        Instantiate(Ring, ringSpawnPos, Quaternion.Euler(0,0,90));
+
+        //Testing "Revive"
+        downXPos = 8.0f;
+        downYPos = 5.0f;
+        downZPos = Random.Range(38.0f, 160.0f);
+        downPos = new Vector3(downXPos, downYPos, downZPos);
+        ringSpawnPos = new Vector3(downXPos, 4.11f, downZPos);
+        Instantiate(DownPlayer, downPos, Quaternion.Euler(90, 0, 0));
+        Instantiate(ReviveRing, ringSpawnPos, Quaternion.Euler(0, 0, 90));
+
     }
 
     // Update is called once per frame
