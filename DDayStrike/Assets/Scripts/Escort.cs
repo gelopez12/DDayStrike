5fe576dbdf61b4142a8b423f7e05913b5ac7d33e
gelopez12 <148307276+gelopez12@users.noreply.github.com>
2023-12-10 20:06:19 -0600

    Changes

--- DDayStrike/Assets/Scripts/Escort.cs
+++ DDayStrike/Assets/Scripts/Escort.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class Escort : MonoBehaviour
 {
     // Start is called before the first frame update
+/*
+
+    private Animator Escorting;
+    public bool Moving;
+
     void Start()
     {
-        
+        Moving = anotherpain.escort;
+        anotherpain = Guy.GetComponent<MissionController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        f(Moving == true)
+        {
+            Escort.SetBool("IsWalking", true);
+        }
+        else
+        {
+            Escort.SetBool("IsWalking", false);
+        }
     }
+*/
 }
