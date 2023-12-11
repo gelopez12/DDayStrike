f1dc7b24d0948cb1d30805a64e01a4ac4b014e10
gelopez12 <148307276+gelopez12@users.noreply.github.com>
2023-12-11 08:44:41 -0600

    a lot of new stuff

--- DDayStrike/Assets/Scripts/Revive.cs
+++ DDayStrike/Assets/Scripts/Revive.cs
@@ -7,9 +7,7 @@ public class Revive : MonoBehaviour
     MissionController pain;  
     public GameObject DGuy;
     public bool Revived;
-    private Animator animator;
-    
-   
+    private Animator animator;  
 
     // Start is called before the first frame update
     void Start()
